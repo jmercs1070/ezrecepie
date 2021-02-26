ea52b2cebd17d5dc27011ebd991b855608b70c03
sunset chan <72767320+SunsetChan@users.noreply.github.com>
2021-02-26 10:06:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+ 
     const input = document.getElementById("userInput")
 
     const btn = document.getElementById("button")
@@ -12,16 +13,19 @@
             }
 
     })
-
-
     const currentIngredients = []
 
 
     //submitbutton function
     function clickedOn()
     {
-
-    // creates ingredient button and displays on the page
+    //Checks if there is any ingredients
+    if(input.value.length === 0){
+        alert("Add an Ingredient before clicking this button!")
+        return;
+    }
+    
+        //creates button and appends onto screen
         const newButton = document.createElement('button')
         newButton.innerHTML = input.value
         body.appendChild(newButton)
@@ -30,28 +34,65 @@
         currentIngredients.push(input.value)
         console.log(currentIngredients)
 
-        input.value = " "
-        }
+        //Header Variables
+        const apikey = "&apiKey=18efb75d9bc148839a7c86210c422a0c"
+        let ApiUrl = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + input.value + apikey
 
+
+
+        //establish connection to the API
+        
+        
+        const userAction = async () => {
+            const response = await fetch(ApiUrl);
+            const myJson = await response.json()
+        
+        console.log(myJson)
+        input.value = " "
+
+        
+        
+  //END OF clickedOn Function 
+    
         //When recipies are revealed aka when are you done button is clicked
         const areYouDoneButton = document.getElementById('areYouDoneButton')
+
         areYouDoneButton.addEventListener('click', ()=>{
+
             //Checks if there is no ingredients added
             if(currentIngredients.length === 0){
                 alert("You don't have any ingredients, make sure to add some!")
-                body.appendChild(areYouDoneButton)
             }
-            
-            //shows recipies
-            switch (input.value) {
-                case 'lol':
-                alert('what\'s so funny?')
-                break;
+            //////////////////////////////////////////////////////////////////////////////////////
+            console.log("pp")
+            const newApiKey = "?apiKey=18efb75d9bc148839a7c86210c422a0c"
+            let newURL = "https://api.spoonacular.com/recipes/" + "/analyzedInstructions" + newApiKey "&stepBreakdown=true"
+            const newIngredientConnection = async () => {
+                const newResponse = await fetch(newURL);
+                const newMyJson = await newResponse.json()
+                console.log(newMyJson)
+            }
+                for(item of myJson){
+                const dataGotBack = []
+                dataGotBack.push(item) 
+                console.log(dataGotBack[0].id)
+                let newURL = "https://api.spoonacular.com/recipes/" + dataGotBack[0].id +  "/analyzedInstructions" + newApiKey + "&stepBreakdown=true"
+                newIngredientConnection()
+            }
 
-                case 'change background':
                 
-            }
-        })
+                }
+            
+            
+    
+        )}
+        userAction()
+        
+    
+    }           
+        //end of add eventlistener
+    
+
         
             
     
